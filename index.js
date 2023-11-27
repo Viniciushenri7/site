c18b523d1076565d336afd660a09de07d004a3e7
Viniciushenri7 <148489330+Viniciushenri7@users.noreply.github.com>
2023-11-27 15:59:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let saldo=2000;
+let valordacompra = Number(prompt("Digite o valor da compra"))
+let total = saldo-valordacompra;
+document.write("Compra ocorreu corretamente " + "Agora seu saldo é " + total )
+
